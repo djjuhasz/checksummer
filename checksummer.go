e21492874405c01ef9c7a8026eef71df0a785459
David Juhasz <djjuhasz@gmail.com>
2021-02-13 13:55:44 -0800

    Combine file open and hashing functions
    Also:
    - Output file/checksum errors, but don't kill script
    - Rename functions

--- checksummer.go
+++ checksummer.go
@@ -9,50 +9,53 @@ import (
 	"path/filepath"
 )
 
-func openFile(path string) (fp *os.File) {
+func getFileHash(path string) (sum []byte, err error) {
 	fp, err := os.Open(path)
+
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
-	return fp
-}
+	defer fp.Close()
 
-func getHash(fp *os.File) []byte {
 	h := sha256.New()
 	if _, err := io.Copy(h, fp); err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	sum := h.Sum(nil)
 
-	return sum
+	sum = h.Sum(nil)
+
+	return sum, err
 }
 
-func processFile(path string, info os.FileInfo, err error) error {
-
+func checksumFile(path string, info os.FileInfo, err error) error {
 	// Skip directories
 	if info.IsDir() {
 		return err
 	}
 
-	fp := openFile(path)
-	defer fp.Close()
+	hash, err := getFileHash(path)
 
-	hash := getHash(fp)
+	// Log errors and keep going
+	if err != nil {
+		log.Println(err)
 
-	fmt.Printf("File: %s, size: %d B, checksum: %x\n", path, info.Size(), hash)
+		return nil
+	}
+
+	log.Printf("File: %s, size: %d B, checksum: %x\n", path, info.Size(), hash)
 
 	return err
 }
 
-func scandir(dirname string) {
-	fmt.Println("Scanning directory: ", dirname)
+func processDir(dirname string) {
+	fmt.Printf("Checksumming files in \"%s\"", dirname)
 
-	filepath.Walk(dirname, processFile)
+	filepath.Walk(dirname, checksumFile)
 }
 
 func main() {
 	var dirname string = "D:\\Photos\\Photos\\2020 Calendar"
 
-	scandir(dirname)
+	processDir(dirname)
 }
